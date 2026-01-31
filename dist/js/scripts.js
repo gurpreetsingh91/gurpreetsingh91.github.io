5be7aa61b7dbe824a84ba56f419a2f2e0aa6c34d
Gurpreet Singh <gurpreet.singhbagga@appdirect.com>
2026-01-31 16:06:02 -0500

    Updates

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2023 Start Bootstrap
+* Copyright 2013-2026 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
 */
 //
